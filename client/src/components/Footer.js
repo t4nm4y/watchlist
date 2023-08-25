71fc29abb0bfd95b8313d8868a262c3e39e093bd
Your Tanmay <00tanmay@gmail.com>
2023-08-26 01:58:28 +0530

    added my github link in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,9 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <h3>Crafted with ❤️ by Tanmay</h3>
+      <h3>
+        Crafted with ❤️ by &nbsp;<a href="https://github.com/t4nm4y" target="_blank">Tanmay</a>
+      </h3>
     </div>
   )
 }
